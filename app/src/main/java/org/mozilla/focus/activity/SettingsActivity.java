b4faab56d55320cb8e288ccb5743b34be31cdfe1
Nevin Chen <cnevinc@livemail.tw>
2018-04-17 18:05:18 +0800

    Fix #346 wip, add more thoughts

--- app/src/main/java/org/mozilla/focus/activity/SettingsActivity.java
+++ app/src/main/java/org/mozilla/focus/activity/SettingsActivity.java
@@ -4,18 +4,28 @@
 
 package org.mozilla.focus.activity;
 
+import android.content.BroadcastReceiver;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.ActionBar;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 
 import org.mozilla.focus.R;
 import org.mozilla.focus.locale.LocaleAwareAppCompatActivity;
 import org.mozilla.focus.settings.SettingsFragment;
+import org.mozilla.focus.widget.DefaultBrowserPreference;
 
 public class SettingsActivity extends LocaleAwareAppCompatActivity {
     public static final int ACTIVITY_RESULT_LOCALE_CHANGED = 1;
+    //TODO: naming should change
+    private BroadcastReceiver receiver;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -51,5 +61,57 @@ public class SettingsActivity extends LocaleAwareAppCompatActivity {
     @Override
     public void applyLocale() {
         setTitle(R.string.menu_settings);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        receiver = new BroadcastReceiver() {
+            @Override
+            public void onReceive(Context context, Intent intent) {
+                Log.d("nevin", "onReceive() called with: context = [" + context + "], intent = [" + intent + "]");
+                final String component = "org.mozilla.focus.activity.WelcomeActivity";
+
+                // TODO: check component here, we only care about Welcome/Fake activity
+                // TODO: Maybe show a dialog here cause we don't want the activity to unregister this reciever.
+                // TODO: Or maybe we can move it somewhere else.... or even just don't care.
+                // we are receiving a disable change event
+                if (DefaultBrowserPreference.isComponentEnabled(context.getPackageManager(), context.getPackageName(), component)) {
+                    // now it's time to let the user choose again
+                    DefaultBrowserPreference.makeUsDefaultByOpenAnUrl(SettingsActivity.this);
+                } else {
+                    // we are receiving an enable change event, now disable it
+                    context.getPackageManager().setComponentEnabledSetting(new ComponentName(context, component), PackageManager.COMPONENT_ENABLED_STATE_DEFAULT, PackageManager.DONT_KILL_APP);
+                }
+
+
+                // for debug
+
+//                if (intent != null) {
+//                    if (intent.getExtras() != null) {
+//                        for (String s : intent.getExtras().keySet()) {
+//                            Log.d("nevin", s+"===="+intent.getExtras().get(s));
+//                        }
+//                    }
+//                }
+            }
+        };
+
+        // we need below filter to listen to fake activity enable event
+        final IntentFilter filter = new IntentFilter(Intent.ACTION_PACKAGE_CHANGED);
+        filter.addAction(Intent.ACTION_PACKAGE_REPLACED);
+        filter.addAction(Intent.ACTION_PACKAGE_REMOVED);
+        filter.addAction(Intent.ACTION_PACKAGE_ADDED);
+
+
+        filter.addDataScheme("package");
+
+        registerReceiver(receiver, filter);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        unregisterReceiver(receiver);
     }
 }
--- app/src/main/java/org/mozilla/focus/widget/DefaultBrowserPreference.java
+++ app/src/main/java/org/mozilla/focus/widget/DefaultBrowserPreference.java
@@ -10,6 +10,8 @@ import android.content.ActivityNotFoundException;
 import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.ComponentInfo;
+import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Build;
@@ -24,6 +26,10 @@ import org.mozilla.focus.utils.Browsers;
 import org.mozilla.focus.utils.IntentUtils;
 import org.mozilla.focus.utils.Settings;
 import org.mozilla.focus.utils.SupportUtils;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 @TargetApi(Build.VERSION_CODES.N)
 public class DefaultBrowserPreference extends Preference {
@@ -60,19 +66,84 @@ public class DefaultBrowserPreference extends Preference {
     @Override
     protected void onClick() {
         final Context context = getContext();
-        // fire an intent and start related activity immediately
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
-            openDefaultAppsSettings(context);
-        } else {
-//            openSumoPage(context);
-            context.getPackageManager().setComponentEnabledSetting(new ComponentName(context, "org.mozilla.focus.activity.WelcomeActivity"), PackageManager.COMPONENT_ENABLED_STATE_ENABLED, PackageManager.DONT_KILL_APP);
-            Intent intent = new Intent(Intent.ACTION_VIEW);
-            String url = SupportUtils.getSumoURLForTopic(context, "rocket-default");
-            intent.setData(Uri.parse(url));
-            context.startActivity(intent);
-            context.getPackageManager().setComponentEnabledSetting(new ComponentName(context, "org.mozilla.focus.activity.WelcomeActivity"), PackageManager.COMPONENT_ENABLED_STATE_DEFAULT, PackageManager.DONT_KILL_APP);
 
+
+
+//        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+//            openDefaultAppsSettings(context);
+//            return;
+//        }
+//        // maybe remove this
+//        openSumoPage(context);
+
+
+
+// method 1 ====== just like UC Browser =====
+// this will open the app setting for current default browser, so you can revoke it.
+// the second time you click it, it'll show a chooser to let users make as default
+//        Browsers browsers = new Browsers(getContext(), "https://mozilla.org");
+//        if (Browsers.isDefaultBrowser(context)) {
+//            if (browsers.getDefaultBrowser() != null) {
+//                // let the user to revoke the original default browser
+//                final Intent i = new Intent(android.provider.Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
+//                i.addCategory(Intent.CATEGORY_DEFAULT);
+//                i.setData(Uri.parse("package:com.UCMobile.intl"));
+//                context.startActivity(i);
+//            } else {
+//
+//                // now it's time to set ourselves default
+//                makeUsDefaultByOpenAnUrl(context);
+//            }
+//
+//        }
+//
+//        final String component = "org.mozilla.focus.activity.WelcomeActivity";
+//        final boolean componentEnabled = isComponentEnabled(context.getPackageManager(), context.getPackageName(), component);
+//        if (componentEnabled) {
+//            context.getPackageManager().setComponentEnabledSetting(new ComponentName(context, component), PackageManager.COMPONENT_ENABLED_STATE_DEFAULT, PackageManager.DONT_KILL_APP);
+//
+//        } else {
+//            context.getPackageManager().setComponentEnabledSetting(new ComponentName(context, component), PackageManager.COMPONENT_ENABLED_STATE_ENABLED, PackageManager.DONT_KILL_APP);
+//
+//        }
+
+// method 2 ====== This is wired. There'll be more than one default browser if you
+// first choose A browser then choose B browser =====
+//        PackageManager p = context.getPackageManager();
+//        ComponentName cN = new ComponentName(c, component);
+//        p.setComponentEnabledSetting(cN,
+//                PackageManager.COMPONENT_ENABLED_STATE_ENABLED,
+//                PackageManager.DONT_KILL_APP);
+//
+//        Intent selector = new Intent(Intent.ACTION_VIEW);
+//        selector.addCategory(Intent.CATEGORY_BROWSABLE);
+//        selector.addCategory(Intent.CATEGORY_DEFAULT);
+//        selector.setData(Uri.parse("https://google.com"));
+//        context.startActivity(selector);
+//        p.setComponentEnabledSetting(cN,
+//                PackageManager.COMPONENT_ENABLED_STATE_DISABLED,
+//                PackageManager.DONT_KILL_APP);
+
+        // method 3 ==== I'd prefer this one
+        final String component = "org.mozilla.focus.activity.WelcomeActivity";
+        final PackageManager packageManager = context.getPackageManager();
+
+        // if the component is enabled, disable it.
+        if (isComponentEnabled(packageManager, context.getPackageName(), component)) {
+            // this should never happen
+            // context.getPackageManager().setComponentEnabledSetting(new ComponentName(context, component), PackageManager.COMPONENT_ENABLED_STATE_DEFAULT, PackageManager.DONT_KILL_APP);
+        } else {
+            context.getPackageManager().setComponentEnabledSetting(new ComponentName(context, component), PackageManager.COMPONENT_ENABLED_STATE_ENABLED, PackageManager.DONT_KILL_APP);
+            // we'll wait for setting activity's BroadcastReceiver to try set as default again.
         }
+
+    }
+
+    public static void makeUsDefaultByOpenAnUrl(Context context) {
+        final String sumo = SupportUtils.getSumoURLForTopic(context, "rocket-default");
+        final Intent i = new Intent(Intent.ACTION_VIEW);
+        i.setData(Uri.parse(sumo));
+        context.startActivity(i);
     }
 
     private void openDefaultAppsSettings(Context context) {
@@ -88,6 +159,45 @@ public class DefaultBrowserPreference extends Preference {
     private void openSumoPage(Context context) {
         final Intent intent = InfoActivity.getIntentFor(context, SupportUtils.getSumoURLForTopic(context, "rocket-default"), getTitle().toString());
         context.startActivity(intent);
+    }
+
+    public static boolean isComponentEnabled(PackageManager pm, String pkgName, String clsName) {
+        ComponentName componentName = new ComponentName(pkgName, clsName);
+        int componentEnabledSetting = pm.getComponentEnabledSetting(componentName);
+
+        switch (componentEnabledSetting) {
+            case PackageManager.COMPONENT_ENABLED_STATE_DISABLED:
+                return false;
+            case PackageManager.COMPONENT_ENABLED_STATE_ENABLED:
+                return true;
+            case PackageManager.COMPONENT_ENABLED_STATE_DEFAULT:
+            default:
+                // We need to get the application info to get the component's default state
+                try {
+                    PackageInfo packageInfo = pm.getPackageInfo(pkgName, PackageManager.GET_ACTIVITIES
+                            | PackageManager.GET_RECEIVERS
+                            | PackageManager.GET_SERVICES
+                            | PackageManager.GET_PROVIDERS
+                            | PackageManager.GET_DISABLED_COMPONENTS);
+
+                    List<ComponentInfo> components = new ArrayList<>();
+                    if (packageInfo.activities != null) Collections.addAll(components, packageInfo.activities);
+                    if (packageInfo.services != null) Collections.addAll(components, packageInfo.services);
+                    if (packageInfo.providers != null) Collections.addAll(components, packageInfo.providers);
+
+                    for (ComponentInfo componentInfo : components) {
+                        if (componentInfo.name.equals(clsName)) {
+                            return componentInfo.isEnabled();
+                        }
+                    }
+
+                    // the component is not declared in the AndroidManifest
+                    return false;
+                } catch (PackageManager.NameNotFoundException e) {
+                    // the package isn't installed on the device
+                    return false;
+                }
+        }
     }
 
 }
